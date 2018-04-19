139dd96576035028838ac21c0e94251e083656d1
junsulime <vfv0121@gmail.com>
2018-04-19 14:42:45 +0900

    [Algospot] APSS work
    1) prime number finder
    2) binary search
    3) sliding window average

---
+++ algospot/letscode/sliding_window.py
@@ -0,0 +1,36 @@
+def sliding_window_average_list(l, window_size):
+	"""
+	-Description-
+
+	With input(list, window_size)
+	find sliding window average list
+
+	1) find first sliding window partial_sum
+	2) iterate ... another window partial_sum by 1)'s value
+	3) keep going
+
+	-Time Complexity-
+
+	O(len(l))
+	"""
+	average_list = list()
+	
+	if len(l) <= window_size:
+		average_list.append(sum(l))
+		return average_list
+
+	partial_sum = 0
+	for i in range(window_size):
+		partial_sum += l[i]
+	
+	average_list.append(partial_sum / window_size)
+	for i in range(window_size, len(l)):
+		partial_sum -= l[i-window_size]
+		partial_sum += l[i]
+
+		average_list.append(partial_sum / window_size)
+	
+	return average_list
+
+if __name__ == '__main__':
+	pass
