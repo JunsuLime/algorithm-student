0f8e418441707954eedbb687c65499e8e792c3d5
junsulime <vfv0121@gmail.com>
2018-04-23 13:57:22 +0900

    [Work] add description about representing graph

--- simple/graph.py
+++ simple/graph.py
@@ -1,4 +1,17 @@
-
+# Three basic ways to represent graph in memory
+# 1) matrix
+# 2) object and pointer
+# 3) adjacent list
+# 
+# Matrix
+# memory so large M*N
+# random access of i to j fast
+#
+# Object and Pointer
+# memory smaller than matrix, custom data setting good
+#
+# Adjacent list
+# memory smaller than Object and pointer, simplest
 
 class Graph(object):
 	pass
