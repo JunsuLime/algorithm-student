37ad41af8905385a6d15a884270f1f8f8e52450b
junsulime <vfv0121@gmail.com>
2018-04-27 23:12:24 +0900

    [Problem solving] solve picnic
    complete search problem with recursively find all case
    fix current state and keep going

---
+++ algospot/picnic.py
@@ -0,0 +1,81 @@
+import itertools
+
+
+test_case = int(input())
+
+
+def set_copy(s):
+	"""
+	util function
+	python set copy function
+
+	@param s: source set
+	@return: copied set
+	"""
+	new_set = set()
+	for e in s:
+		new_set.add(e)
+	return new_set
+
+
+def work():
+	# s_num: student number, p_num: pair num
+	s_num, p_num = map(int, input().split())
+
+	# make list of set length student number
+	# this will save friend ship
+	# like 0's friend: friend_map[0] = {1, 2, 4}
+	friend_map = [set() for _ in range(s_num)]
+
+	# enumerate - (idx, element) iteration
+	current_base = 0
+	for idx, s in enumerate(map(int, input().split())):
+		if idx % 2 == 0:
+			current_base = s
+		else:
+			# if a is b's friend, b is a's friend too.
+			friend_map[current_base].add(s)
+			friend_map[s].add(current_base)
+	
+	def matching():
+		"""
+		return number of matching case.
+		"""
+		def __matching_internal(m, tried_pair):
+			"""
+			return match_count of current state (tried_pair)
+			@param m: current base matcher
+			@return: match count
+			"""
+			# if all pair is mathed -> return 1: success
+			if len(tried_pair) == s_num:
+				return 1
+			# if match base is already matched student -> return 0: failure
+			if m in tried_pair:
+				return 0
+
+			match_count = 0
+			# only m's friend can be matched
+			for f in friend_map[m]:
+				if f in tried_pair:
+					continue
+				pair = set_copy(tried_pair)
+				pair.add(m)
+				pair.add(f)
+				next_m = m+1
+				while next_m in pair:
+					next_m += 1
+				# get fastest number matchable friend
+				match_count += __matching_internal(next_m, pair)
+
+			return match_count
+	
+		# start matching work from 0
+		return __matching_internal(0, set())
+	
+	return matching()
+
+
+for _ in range(test_case):
+	print(work())
+
