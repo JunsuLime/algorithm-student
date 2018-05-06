fdf6455412284f987a3354de852d915c65448e06
junsulime <vfv0121@gmail.com>
2018-05-06 14:16:02 +0900

    [Work] tree.py skeleton code is added

---
+++ simple/tree.py
@@ -0,0 +1,21 @@
+# AVL tree (balanced binary tree)
+# Splay tree
+# Red-Black tree
+# B tree
+# B+tree
+
+class AVLTree(object):
+	pass
+
+class SplayTree(object):
+	pass
+
+class RBTree(object):
+	pass
+
+class BTree(object):
+	pass
+
+class BPlusTree(object):
+	pass
+
